8461d2a0b1eba7383a69b81367f46c39fe5e13e2
Eric Wu <wueric@stanford.edu>
2021-11-04 13:52:07 -0700

    adding a few test cases

---
+++ _export_data.py
@@ -0,0 +1,46 @@
+import os
+import visionloader as vl
+import bin2py
+
+import numpy as np
+
+ABOUT_STR = 'Generate data for test cases for fast 1D correlations'
+
+TEST_DATASET = '/Volumes/Data/2018-08-07-1/data000'
+TEST_DATASET_NAME = 'data000'
+
+SPSORT_PATH = '/Volumes/Analysis/2018-08-07-1/data000'
+
+TEMPLATE_CELL_IDS = [541, 3106, 3607, 3018, 1401]
+
+TESTCASE_PATH = 'testcases'
+MULTIDATA_MULTICHAN_FNAME = 'multidata_multichan.npy'
+MULTICHAN_FILTERS_FNAME = 'multifilters.npy'
+
+N_SAMPLES_DATA = 20001
+DATA_START_SAMPLES = [0, 70000, 1000000, 2000000, 5000000]
+
+if __name__ == '__main__':
+
+    os.makedirs(TESTCASE_PATH, exist_ok=True)
+    data_file_path = os.path.join(TESTCASE_PATH, MULTIDATA_MULTICHAN_FNAME)
+
+    with bin2py.PyBinFileReader(TEST_DATASET, TEST_DATASET_NAME, is_row_major=True) as pbfr:
+
+        n_channels = pbfr.num_electrodes
+
+        output_npy_buffer = np.zeros((len(DATA_START_SAMPLES), n_channels, N_SAMPLES_DATA), dtype=np.float64)
+        for i, offset in enumerate(DATA_START_SAMPLES):
+            output_npy_buffer[i,...] = pbfr.get_data(offset, N_SAMPLES_DATA)
+
+    np.save(data_file_path, output_npy_buffer, allow_pickle=True)
+
+    filter_file_path = os.path.join(TESTCASE_PATH, MULTICHAN_FILTERS_FNAME)
+    analysis_dset = vl.load_vision_data(SPSORT_PATH, TEST_DATASET_NAME, include_params=True, include_ei=True)
+    ei_nchan, ei_nsamples = analysis_dset.get_ei_for_cell(TEMPLATE_CELL_IDS[0]).ei.shape
+    ei_buffer = np.zeros((len(TEMPLATE_CELL_IDS), n_channels, ei_nsamples), dtype=np.float64)
+
+    for i, cell_id in enumerate(TEMPLATE_CELL_IDS):
+        ei_buffer[i, ...] = analysis_dset.get_ei_for_cell(cell_id).ei
+
+    np.save(analysis_dset, ei_buffer, allow_pickle=True)
---
+++ tests.py
@@ -0,0 +1,74 @@
+from fastcorr import corr1d
+import numpy as np
+
+import os
+
+TESTCASE_PATH = 'testcases'
+MULTIDATA_MULTICHAN_FNAME = 'multidata_multichan.npy'
+MULTICHAN_FILTERS_FNAME = 'multifilters.npy'
+
+FILTER_PATH = os.path.join(TESTCASE_PATH, MULTICHAN_FILTERS_FNAME)
+DATA_PATH = os.path.join(TESTCASE_PATH, MULTIDATA_MULTICHAN_FNAME)
+
+FILTERS = np.load(FILTER_PATH)
+DATA = np.load(FILTER_PATH)
+
+def test_corr1D_AA_double():
+
+    test_data = DATA[0, 35, :].astype(np.float64)
+    test_filter = FILTERS[0, 35, :].astype(np.float64)
+
+    comparison = np.correlate(test_data, test_filter, mode='valid') # type: np.ndarray
+    my_output = corr1d.short_filter_correlate1D(test_data, test_filter) # type: np.ndarray
+
+    assert my_output.dtype == np.float64, 'data type should be np.float64'
+
+    assert comparison.shape == my_output.shape, 'shape is {0}, should be {1}'.format(my_output.shape,
+                                                                                     comparison.shape)
+
+    assert np.allclose(comparison, my_output)
+
+
+def test_corr1D_AA_float():
+    test_data = DATA[0, 35, :].astype(np.float32)
+    test_filter = FILTERS[0, 35, :].astype(np.float32)
+
+    comparison = np.correlate(test_data, test_filter, mode='valid')
+    my_output = corr1d.short_filter_correlate1D(test_data, test_filter)
+
+    assert my_output.dtype == np.float32, 'data type should be np.float32'
+
+    assert comparison.shape == my_output.shape, 'shape is {0}, should be {1}'.format(my_output.shape,
+                                                                                     comparison.shape)
+
+    assert np.allclose(comparison, my_output)
+
+def test_corr1D_AB_double():
+
+    test_data = DATA[0, 35, 1000:2001].astype(np.float64)
+    test_filter = FILTERS[0, 35, :].astype(np.float64)
+
+    comparison = np.correlate(test_data, test_filter, mode='valid') # type: np.ndarray
+    my_output = corr1d.short_filter_correlate1D(test_data, test_filter) # type: np.ndarray
+
+    assert my_output.dtype == np.float64, 'data type should be np.float64'
+
+    assert comparison.shape == my_output.shape, 'shape is {0}, should be {1}'.format(my_output.shape,
+                                                                                     comparison.shape)
+
+    assert np.allclose(comparison, my_output)
+
+
+def test_corr1D_AB_float():
+    test_data = DATA[0, 35, 1000:2001].astype(np.float32)
+    test_filter = FILTERS[0, 35, :].astype(np.float32)
+
+    comparison = np.correlate(test_data, test_filter, mode='valid')
+    my_output = corr1d.short_filter_correlate1D(test_data, test_filter)
+
+    assert my_output.dtype == np.float32, 'data type should be np.float32'
+
+    assert comparison.shape == my_output.shape, 'shape is {0}, should be {1}'.format(my_output.shape,
+                                                                                     comparison.shape)
+
+    assert np.allclose(comparison, my_output)
